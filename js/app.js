f7680037afcf34d5d2ddba8716244877b3826c15
Keyen331 <banayos.kay@gmail.com>
2016-12-16 08:55:53 -1000

    uploading

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function red() {
+  var changeColor = document.getElementById("box");
+  changeColor.innerHTML = "red";
+};
+
+function myFunction() {
+  var changeWord = document.getElementById("box");
+  changeWord.innerHTML = "Bye World";
+}
